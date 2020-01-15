bf4f717c6647df84e1e06fd24e1f6c6e9a5f8770
steele-cjf <steele.chen@things-matrix.com>
2020-01-15 19:34:00 +0800

    feat(cache)

--- view/components/googleMap.js
+++ view/components/googleMap.js
@@ -1,46 +1,123 @@
-import React, { Component } from 'react';
-import { Container, Text, Header, Button, Left, Icon, Body } from 'native-base';
-import { StyleSheet } from 'react-native'
-import MapView from 'react-native-maps';
-export default class googleMap extends Component {
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Dimensions,
+  TouchableOpacity,
+  Platform
+} from 'react-native';
+
+import MapView, { Marker, ProviderPropType } from 'react-native-maps';
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+let id = 0;
+
+function randomColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+class DefaultMarkers extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      region: {
+        latitude: LATITUDE,
+        longitude: LONGITUDE,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+      },
+      markers: [],
+    };
+  }
+
+  onMapPress(e) {
+    this.setState({
+      markers: [
+        ...this.state.markers,
+        {
+          coordinate: e.nativeEvent.coordinate,
+          key: id++,
+          color: randomColor(),
+        },
+      ],
+    });
+  }
+
   render() {
     return (
-      <Container>
-        <Header>
-          <Left>
-            <Button  onPress={() => this.props.navigation.openDrawer()}>
-              <Icon name='more' />
-            </Button>
-          </Left>
-          <Body/>
-        </Header>
+      <View style={styles.container}>
         <MapView
+          mapType={Platform.OS == "android" ? "none" : "standard"}
+          provider={this.props.provider}
           style={styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}/>
-      </Container>
+          initialRegion={this.state.region}
+          onPress={e => this.onMapPress(e)}
+        >
+          {this.state.markers.map(marker => (
+            <Marker
+              key={marker.key}
+              coordinate={marker.coordinate}
+              pinColor={marker.color}
+            />
+          ))}
+        </MapView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={() => this.setState({ markers: [] })}
+            style={styles.bubble}
+          >
+            <Text>Tap to create a marker of random color2</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
 
+DefaultMarkers.propTypes = {
+  provider: ProviderPropType,
+};
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'flex-end',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
- map: {
-    position: 'absolute',
-    top: 100,
-    left: 0,
-    height: 800,
-    width: 300,
-    bottom: 20,
-    right: 200
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  bubble: {
+    backgroundColor: 'rgba(255,255,255,0.7)',
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20,
+  },
+  latlng: {
+    width: 200,
+    alignItems: 'stretch',
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: 'center',
+    marginHorizontal: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginVertical: 20,
+    backgroundColor: 'transparent',
   },
 });
+
+export default DefaultMarkers;
